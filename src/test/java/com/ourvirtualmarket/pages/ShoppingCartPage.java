58e61ac89d0aa22fe6a2e46d4f6a8b63215ee067
aysezynp <128591659+aysezynp@users.noreply.github.com>
2023-09-30 04:00:43 +0300

    First commit

--- src/test/java/com/ourvirtualmarket/pages/ShoppingCartPage.java
+++ src/test/java/com/ourvirtualmarket/pages/ShoppingCartPage.java
@@ -1,20 +1,36 @@
 package com.ourvirtualmarket.pages;
 
 
+import com.ourvirtualmarket.utilities.BrowserUtils;
 import com.ourvirtualmarket.utilities.Driver;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class ShoppingCartPage extends BasePage{
+
+    @FindBy(xpath = "//a[@class='btn btn-primary']")
+    public WebElement checkoutButton;
 
     public void verifyProductAddedToCart(String productName){
         WebElement addedProductInTheShoppingCart = Driver.get().findElement(By.xpath("//td[contains(text()," +
                 "'"+productName+"')]"));
-        String actualProductNameInShoppingCart=productName;
-        String expectedProductNameInShoppingCart=addedProductInTheShoppingCart.getText();
-        Assert.assertTrue(expectedProductNameInShoppingCart.contains(actualProductNameInShoppingCart));
+        String expectedProductNameInShoppingCart=productName;
+        String actualProductNameInShoppingCart=addedProductInTheShoppingCart.getText();
+        Assert.assertTrue(actualProductNameInShoppingCart.contains(expectedProductNameInShoppingCart));
     }
+
+   public void verifyCheckoutButton(String checkoutBtn){
+        String actualCheckoutBtn=checkoutButton.getText();
+        String expectedCheckoutBtn=checkoutBtn;
+        Assert.assertEquals(expectedCheckoutBtn,actualCheckoutBtn);
+   }
+
+   public void clickTheCheckoutBtn(){
+       BrowserUtils.clickWithJS(checkoutButton);
+       BrowserUtils.waitFor(2);
+   }
 
 
 }
